899afddb809bd12166c245bd0d27ef09f512638c
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-22 22:28:33 +0900

    Update creators_controller.rb

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,2 +1,23 @@
 class CreatorsController < ApplicationController
+
+  def new
+    @creator = Creator.new
+  end
+
+  def create
+    @creator = Creator.new(creator_params)
+
+    if @creator.save
+      redirect_to @creator
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def creator_params
+    params.require(:creator).permit(:name)
+  end
 end
