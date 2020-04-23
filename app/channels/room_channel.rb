5c1c78ba062d5e76f9416326db7e1fccee1f7d3e
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-23 23:28:19 +0900

    Create room_channel.rb

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,15 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    # このparams["room_id"]は上のroom.coffeeでsubscriptionを作る際に渡しているのがポイント。
+    stream_from "room_channel_#{params['room_id']}"
+  end
+
+  def speak(data)
+    # ここでRoomとMessageが紐付けられる。
+    unless current_user.creator
+      Message.create content: data['message'], is_user: true, room_id: params['room_id'], user_id: current_user.id
+    else
+      Message.create content: data['message'], is_user: false, room_id: params['room_id'], creator_id: current_user.creator.id
+    end
+  end
+end
