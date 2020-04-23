e9eaf43d4bae2cc1be13cb14fe32cc26f9972bea
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-23 23:29:09 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ApplicationRecord
-  belongs_to :room
+  belongs_to :room, optional: true
+  belongs_to :user, optional: true
+  belongs_to :creator, optional: true
+  # メッセージの保存に成功したらBroadCastJobに投げる
+  after_create_commit { BroadCastMessageJob.perform_later self }
+
 end
