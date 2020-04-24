e3d971ac281ea72d5f4e729b60976813ed3f0d9a
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-24 21:35:55 +0900

    Update room_channel.rb

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -7,9 +7,9 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     # ここでRoomとMessageが紐付けられる。
     unless current_user.creator
-      Message.create content: data['message'], is_user: true, room_id: params['room_id'], user_id: current_user.id
+      Message.create content: data['message'], is_user: true, room_id: params['room_id'], user_id: current_user.id, creator_id: params['creator_id']
     else
-      Message.create content: data['message'], is_user: false, room_id: params['room_id'], creator_id: current_user.creator.id
+      Message.create content: data['message'], is_user: false, room_id: params['room_id'], creator_id: current_user.creator.id, user_id: user.id
     end
   end
 end
